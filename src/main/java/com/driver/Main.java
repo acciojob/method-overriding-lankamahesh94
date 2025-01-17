2938bc54829df20f864ec06d7f3212ab7b4f9a53
lanka <lankamahesh94@gmail.com>
2025-01-17 22:39:45 +0530

    sloved assignment overriding

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,24 @@
 package com.driver;
 
+class A {
+    public String meth() {
+        return "Invoking method from class A";
+    }
+}
+
+class B extends A {
+    @Override
+    public String meth() {
+        return "Method is overridden in Extended class B";
+    }
+}
+
 public class Main {
-  
+    public static void main(String[] args) {
+        // Create an object of class B
+        B objB = new B();
+
+        // Call the overridden method from class B
+        System.out.println(objB.meth());  // Output: "Method is overridden in Extended class B"
+    }
 }
